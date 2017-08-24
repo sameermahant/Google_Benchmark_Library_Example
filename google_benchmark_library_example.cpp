0d77037208006de9999e00f3bc777f119f59b2ff
Sameer Mahant <mahant.sameer@gmail.com>
2017-08-24 21:26:23 +0530

    Final stage code related to my tutorial on Google Benchmark library.
    Final stage code related to my tutorial on Google Benchmark library.

--- google_benchmark_library_example.cpp
+++ google_benchmark_library_example.cpp
@@ -1,9 +1,17 @@
 #include <benchmark/benchmark.h>
 
-// Modify this function as needed
+static void CustomArguments(benchmark::internal::Benchmark* b)
+{
+    for (int i = 11; i <= 15; ++i) {
+        b->Args({i});
+    }
+}
+
 int HashFunction(int number)
 {
-    return number % 1000;
+    // return number % 1000;
+    // return number % 1024;
+    return number & 1023;
 }
 
 void BM_HashFunction(benchmark::State& state)
@@ -13,6 +21,24 @@ void BM_HashFunction(benchmark::State& state)
   }
 }
 
+// Single input
 BENCHMARK(BM_HashFunction)->Args({42});
+
+// Multiple inputs
+BENCHMARK(BM_HashFunction)->Args({1000})->Args({42});
+
+// Range inputs with default multiplier (8)
+BENCHMARK(BM_HashFunction)->Range(1,2048);  // { 1, 8, 64, 512, 2048 }
+
+// Range input with custom multiplier (2)
+BENCHMARK(BM_HashFunction)->RangeMultiplier(2)->Range(1,10); // { 1, 2, 4, 8, 10 }
+
+// Custom Arguments
+BENCHMARK(BM_HashFunction)->Apply(CustomArguments);  // { 11, 12, 13, 14, 15 }
+
+// Different output time units
+BENCHMARK(BM_HashFunction)->Args({42})->Unit(benchmark::kNanosecond); // default
+BENCHMARK(BM_HashFunction)->Args({42})->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_HashFunction)->Args({42})->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN()
--- run.sh
+++ run.sh
@@ -16,6 +16,11 @@ echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governo
 # Running our process on isolated CPU core
 taskset -c 1 ./google_benchmark_library_example
 
+# Different output formats
+# taskset -c 1 ./google_benchmark_library_example --benchmark_format=console  # default
+# taskset -c 1 ./google_benchmark_library_example --benchmark_format=json
+# taskset -c 1 ./google_benchmark_library_example --benchmark_format=csv
+
 # Re-setting CPU performance mode (requires root permission)
 echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 echo "================================================================================"
